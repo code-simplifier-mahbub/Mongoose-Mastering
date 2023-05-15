9c5144f4e9f3e1a267b676e34ad62b20d646df5c
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2023-05-15 23:32:09 +0600

    data get with postman and data format with json formater

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { createUser } from "./user.controller";
+import { createUser, getUsers } from "./user.controller";
 const router = express.Router();
 
-router.get('/', createUser)
+router.get('/get-user', getUsers);
+
+router.post('/create-user', createUser)
 
 
 export default router
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,30 +1,37 @@
+import { IUser } from "./user.interface";
 import User from "./user.model";
 
 
-export const createUserToDB =async () => {
-   
+export const createUserToDB =async (payLoad: IUser)
+:Promise<IUser> => {
     //creare instance
-    const user = await new User({ //here User do query and we know that database query on model  and await here for async
-      id: "424",
-      role: "student",
-      password: "testing",
-      name: {
-        firstName: "Mahbub",
-        middleName: "Hasan",
-        lastName: "Mahbub"
-      },
-      dateOfBirth: "january",
-      gender: "male",
-      email: "hasan@gmail.com",
-      contactNo: "33333333",
-      emergencyContactNo: "333333333",
-      presentAddress: "Bangladesh",
-      permanentAddress: "usa",
-    });
+   
+    const user = new User(payLoad)
     await user.save();
     // console.log(user)
     return user;
     };
 
+    export const getUserFromDB = async() => {
+      const users = await User.find();
+      return users
+    }
 
-    // createUserToDB(); don't call here because when we call route then database logic going to work
+
+    // const user = await new User({ //here User do query and we know that database query on model  and await here for async
+    //   id: "424",
+    //   role: "student",
+    //   password: "testing",
+    //   name: {
+    //     firstName: "Mahbub",
+    //     middleName: "Hasan",
+    //     lastName: "Mahbub"
+    //   },
+    //   dateOfBirth: "january",
+    //   gender: "male",
+    //   email: "hasan@gmail.com",
+    //   contactNo: "33333333",
+    //   emergencyContactNo: "333333333",
+    //   presentAddress: "Bangladesh",
+    //   permanentAddress: "usa",
+    // });
